a7809f4ee1f672e195899804cb5250aa5359e5f1
Khankisiyevravan <revan.xankisiyev.02@mail.ru>
2024-07-10 17:38:28 +0400

    all - finish without footer

--- src/components/Modal/Success.jsx
+++ src/components/Modal/Success.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import "./Success.css";
 import { useTranslation } from 'react-i18next';
 
--- src/pages/BlogDetail.jsx
+++ src/pages/BlogDetail.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect,  useState } from 'react'
 import { useTranslation } from 'react-i18next';
 import { useParams } from 'react-router-dom';
 import { readData } from '../api/dbservice';
