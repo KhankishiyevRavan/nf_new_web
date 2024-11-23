10b6b12a6a96fa61247475b97e5adc00ac3cfcf5
Khankisiyevravan <revan.xankisiyev.02@mail.ru>
2024-11-23 15:12:25 +0400

    update for seo

--- src/components/Modal/Success.jsx
+++ src/components/Modal/Success.jsx
@@ -11,12 +11,12 @@ const Modal = ({ show, closeModal }) => {
         <div id='success_modal' className={`modal ${show ? 'show' : ''}`}>
             <div className="modal_content">
                 <span className="close" onClick={closeModal}>&times;</span>
-                <h1 style={{ textAlign: "center" }}>
+                <h6 style={{ textAlign: "center" }}>
                     <div className="checkmark-circle">
                         <div className="background"></div>
                         <div className="checkmark draw"></div>
                     </div>
-                </h1>
+                </h6>
                 <p style={{ marginTop: "20px" }}>{t('thank_you_message')}</p>
             </div>
         </div>
